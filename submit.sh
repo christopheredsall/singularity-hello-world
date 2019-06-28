72109d1767218aac667935651abc826e35a4d234
Christopher Edsall <chris.edsall@bristol.ac.uk>
2019-06-28 14:04:51 +0000

    don't oversubscribe

--- submit.sh
+++ submit.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
 #SBATCH --job-name="singularity_mpi_hello_world" 
-#SBATCH --nodes=1 
+#SBATCH --nodes=3 
 #SBATCH --ntasks-per-node=2 
 #SBATCH --time=00:10:00 
 
 source /etc/profile.d/modules.sh 
 module use /etc/modulefiles
 module load mpi/openmpi3-x86_64
-mpirun -np 2 --oversubscribe singularity exec mpi-hello-test.img mpi_hello_world
+mpirun -np 6 singularity exec mpi-hello-test.img mpi_hello_world
 
